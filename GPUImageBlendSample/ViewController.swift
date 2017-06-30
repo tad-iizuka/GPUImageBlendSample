79e2ec1e3210f0f62e699aa54629ef59bd2aa8de
tad-iizuka <tad.iizuka@gmail.com>
2017-06-30 12:24:40 +0900

    Initial commit

--- GPUImageBlendSample/ViewController.swift
+++ GPUImageBlendSample/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import GPUImage
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+
+		let alphaBlend = AlphaBlend()
+		alphaBlend.mix = 0.5
+
+		let input1 = PictureInput(image: UIImage.init(named: "IMAGE1.jpg")!)
+		let input2 = PictureInput(image: UIImage.init(named: "IMAGE2.jpg")!)
+		let output = PictureOutput()
+		output.imageAvailableCallback = {image in
+			UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+		}
+		input1 --> alphaBlend
+		input2 --> alphaBlend --> output
+
+		input1.processImage(synchronously: true)
+		input2.processImage(synchronously: true)
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
 }
 
